5977b7afbcde5c2bf65df3bf0e120f8be5b1b3a4
TinasheMTapera <15770644+TinasheMTapera@users.noreply.github.com>
2026-06-13 09:01:11 -0400

    cron job commit @ {runtime}

--- run.R
+++ run.R
@@ -1,9 +1,2 @@
 # render
 quarto::quarto_render()
-
-# git
-repo_path <- here::here()
-repo <- git2r::init(repo_path)
-git2r::add(repo, "*")
-msg <- glue::glue("update website: {Sys.time()}")
-git2r::commit(repo, msg)
